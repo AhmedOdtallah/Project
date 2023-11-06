489ff5157cc0371eee732804e8019f7da3843277
Ahmed <ahmedodtallah@gmail.com>
2023-11-06 11:09:44 -0800

    Small variable rewrite

--- View.java
+++ View.java
@@ -5,7 +5,18 @@ import java.awt.*;
 
 class View{
     public View(){
+
         JFrame frame = new JFrame("Joe Mama LOL");
+        JPanel computersContainer = new JPanel(new GridLayout(2, 4, 15, 12));
+        JButton[] computers = new JButton[8];
+        JPanel northPanel = new JPanel(new GridLayout(1,3));
+        JButton catalogButton = new JButton("PC Catalog");
+        JButton configurator = new JButton("Configurator");
+        JButton closeButton = new JButton("Close");
+        JPanel bottomPanel = new JPanel();
+        JButton addPC = new JButton("CONFIGURE NEW PC");
+        
+        
         final int width = 800;
         final int height = 600;
         frame.setVisible(true);
@@ -14,18 +25,12 @@ class View{
         frame.setResizable(false);
         frame.setDefaultCloseOperation(3);
 
-        JPanel computersContainer = new JPanel(new GridLayout(2, 4, 8, 6));
-        JButton[] computers = new JButton[8];
 
         for(int index = 0; index < 8; index++){
             computers[index] = new JButton("PC " + (index+1));
             computersContainer.add(computers[index]);
         }
 
-        JPanel northPanel = new JPanel(new GridLayout(1,3));
-        JButton catalogButton = new JButton("PC Catalog");
-        JButton configurator = new JButton("Configurator");
-        JButton closeButton = new JButton("Close");
         northPanel.add(catalogButton);
         northPanel.add(configurator);
         northPanel.add(closeButton);
@@ -35,8 +40,6 @@ class View{
         computersContainer.setSize(width, height);
         frame.add(computersContainer, BorderLayout.CENTER);
         
-        JPanel bottomPanel = new JPanel();
-        JButton addPC = new JButton("CONFIGURE NEW PC");
         bottomPanel.add(addPC, BorderLayout.EAST);
         frame.add(bottomPanel, BorderLayout.SOUTH);
     }
