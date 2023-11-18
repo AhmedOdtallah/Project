ca056a0d2c31275b83088abb4ec5731c4359a444
Ahmed <ahmedodtallah@gmail.com>
2023-11-18 14:46:32 -0800

    Changed Component[][] to HashMap in ComponentList

--- ComponentsList.java
+++ ComponentsList.java
@@ -1,32 +1,37 @@
+import java.util.HashMap;
+
 public class ComponentsList {
     
-    public static final Component[][] moboList = {{new Component("A620", 150), new Component("B650M", 175),
-    new Component("B650", 200), new Component("X670", 225), new Component("X670E", 250)},
-    {new Component("B660M", 150), new Component("B660", 175), new Component("Z690M", 200),
-    new Component("Z790", 225), new Component("Z790E", 250)}};
-    
-    public static final Component[][] cpuList = {{new Component("7500F", 125), new Component("7600", 200),
-    new Component("7600X", 225), new Component("7700", 300), new Component("7700X",325), 
-    new Component("7800X", 400), new Component("7800X3D", 425), new Component("7950X", 500)},
-    {new Component("14100F",125), new Component("14400F",150), new Component("13600K",200), 
-    new Component("14600K",300), new Component("13700K",350), new Component("13900K",400), 
-    new Component("13900KS",500), new Component("14900K",550)}};
-    
-    public static final Component[][] gpuList = {{new Component("6700XT",300), new Component("7700XT", 450), 
-    new Component("6800",400), new Component("6800XT", 450), new Component("7800XT",500), 
-    new Component("7900XT",600), new Component("7900XTX",750), new Component("AMD God Card", 5000)},
-    {new Component("4060",300), new Component("4060TI",350), new Component("4060TI 16GB",400), 
-    new Component("4070",600), new Component("4070TI",700), new Component("4080", 1200), 
-    new Component("4090",1600), new Component("4090 TI",2000)}};
-    
-    public static final Component[] ramList = {new Component("DDR5 4800",80), new Component("DDR5 5200",90), 
-    new Component("DDR5 5600",100), new Component("DDR5 6000", 120)};
-    
-    public static final Component[] psuList = {new Component("850 GOLD",100), new Component("850 PLATINUM",125), 
-    new Component("1000 GOLD",125), new Component("1000 PLATINUM",150), new Component("1500 GOLD",175), 
-    new Component("1500 PLATINUM",200), new Component("1500 ELITE", 225), new Component("NUCLEAR REACTOR",5000)};
-    
-    public static final Component[] storageList = {new Component("1 TB",60), new Component("2 TB",120), 
-    new Component("3 TB",180), new Component("4 TB",240)};
- 
+    private HashMap<String, Integer> motherboards = new HashMap<>(){{
+        put("A620", 150); put("B650M", 175); put("B650", 200); 
+        put("X670", 225); put("X670E", 250);
+    }};
+
+    private HashMap<String, Integer> cpus = new HashMap<>(){{
+        put("7500F", 125); put("7600", 200); put("7600X", 225); 
+        put("7700", 300); put("7700X",325); put("7800X", 400); 
+        put("7800X3D", 425); put("7950X", 500);
+    }};
+
+    private HashMap<String, Integer> gpus = new HashMap<>(){{
+        put("6700XT",300); put("7700XT", 450); put("6800XT", 450); 
+        put("7800XT",500); put("7900XT",600); put("7900XTX",750); 
+    }};
+
+    private HashMap<String, Integer> ram = new HashMap<>(){{
+        put("DDR5 4800",80); put("DDR5 5200",90); put("DDR5 5600",100); 
+        put("DDR5 6000", 120);
+    }};
+
+    private HashMap<String, Integer> psu = new HashMap<>(){{
+        put("850 GOLD",100); put("1000 GOLD",125); put("1000 PLATINUM",150); 
+        put("1500 GOLD",175); put("1500 PLATINUM", 200); put("1500 ELITE",225); 
+    }};
+
+    private HashMap<String, Integer> storage = new HashMap<>(){{
+        put("1 TB",60); put("2 TB",120); put("3 TB",180); 
+        put("4 TB",240); 
+    }};
+
+
 }
