88a9af2c4e2bcfa997b09a136c1be48f65f77cbe
Ahmed <ahmedodtallah@gmail.com>
2023-11-29 11:13:53 -0800

    changes to TestProject

--- TestProject.java
+++ TestProject.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class TestProject extends Thread{
     private static View view;
@@ -39,12 +40,11 @@ public class TestProject extends Thread{
             }
             int randomPC = waitTime.nextInt(8);
 
-            if(customer.purchase(view.getComputer(randomPC)) != false){
-                System.out.println( view.getComputer(randomPC).pcType() + " bought by: " + customer.getName());
+            if(customer.purchase(view.getComputer(randomPC)) == true){
+                JOptionPane.showMessageDialog(null, view.getComputer(randomPC).pcType() + " bought by: " + customer.getName() + 
+                "\n" + customer.getName() + " walked out!");
             }
-            System.out.println(customer.getName() + " walked out!");
             customers.remove(grabRandomCustomer);
-            System.out.println(customers.size());
         }
     }
 }
